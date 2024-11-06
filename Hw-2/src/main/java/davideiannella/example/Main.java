b5e2e37726d20bee7b255ac87593d0a6b6d24e94
davideiannella <davideiannella@gmail.com>
2024-11-06 19:09:51 +0100

    Init Main

---
+++ Hw-2/src/main/java/davideiannella/example/Main.java
@@ -0,0 +1,34 @@
+package davideiannella.example;
+
+import davideiannella.example.mylucene.InputManager;
+import davideiannella.example.mylucene.LuceneManager;
+import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.queryparser.classic.QueryParser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main( String[] args ) throws Exception {
+        Document doc1 = new Document();
+        doc1.add(new TextField("titolo", "Come diventare un ingegnere dei dati, Data Engineer?", Field.Store.YES));
+        doc1.add(new TextField("contenuto", "Sembra che oggigiorno tutti vogliano diventare un Data Scientist  ...", Field.Store.YES));
+        doc1.add(new StringField("data", "12 ottobre 2016", Field.Store.YES));
+
+        Document doc2 = new Document();
+        doc2.add(new TextField("titolo", "Curriculum Ingegneria dei Dati - Sezione di Informatica e Automazione", Field.Store.YES));
+        doc2.add(new TextField("contenuto", "Curriculum. Ingegneria dei Dati. Laurea Magistrale in Ingegneria Informatica ...", Field.Store.YES));
+        QueryParser parser = new QueryParser("titolo", new WhitespaceAnalyzer());
+        // Builds query
+        List<Document> docsList = new ArrayList<Document>();
+        docsList.add(doc1);
+        docsList.add(doc2);
+        new LuceneManager().runHw_2(parser,docsList);
+}
+}
